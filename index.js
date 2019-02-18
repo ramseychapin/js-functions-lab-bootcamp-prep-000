7911aac8ef6fed5f81edede0bd2a52228498b0dc
Ramsey Chapin <ramsey@ramseychapin.com>
2019-02-18 20:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ function happyHolidays() {
 
 
 function happyHolidaysTo(name) {
-  var answer = "Happy holidays, ${name}!"
-  return answer
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday, name) {
-  var answer = 'Happy ${holiday}, ${name}!'
-  return answer
+  return 'Happy ${holiday}, ${name}!'
 }
 
 function holidayCountdown(holiday, name) {
-  var answer = 'It\'s ${days} days until ${holiday}!'
-  return answer
+  return 'It\'s ${days} days until ${holiday}!'
 }
